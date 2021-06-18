a18aedfc23c9c9c5c5caef489d711e1a1e91e620
Clifford Morin <cemorin21@gmail.com>
2021-06-18 01:32:03 -0400

    added scss style files

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons"
 
 const Player = () => {
     return(
@@ -8,7 +10,11 @@ const Player = () => {
                 <input type="range" />
                 <p>end time</p>
             </div>
-            <div className="play-control"></div>
+            <div className="play-control">
+                <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
+                <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+                <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
+            </div>
         </div>
     );
 };
