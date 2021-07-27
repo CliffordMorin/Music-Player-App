cca5183a34c9b9b71474db33e9de3aca4dc4e464
Clifford Morin <cemorin21@gmail.com>
2021-07-27 16:23:29 -0400

    added onclick to change each song, and each song style

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, {useState} from 'react';
 //Adding Components
 import Player from './components/Player';
 import Song from './components/Song';
+import Library from './components/Library'
 //Import Styles
 import "./styles/app.scss";
 //Import Util
 import data from "./util";
+
 
 
 function App() {
@@ -16,7 +18,12 @@ function App() {
   return (
     <div className="App">
       <Song currentSong={currentSong}/>
-      <Player setIsPlaying={setIsPlaying} isPlaying={isPlaying} currentSong={currentSong}/>
+      <Player 
+        setIsPlaying={setIsPlaying} 
+        isPlaying={isPlaying} 
+        currentSong={currentSong}
+      />
+      <Library songs={songs} setCurrentSong={setCurrentSong}/>
     </div>
   );
 }
