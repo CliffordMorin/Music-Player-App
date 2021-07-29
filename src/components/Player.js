2730815935e92ea126f062a7ac8192e16ff3649e
Clifford Morin <cemorin21@gmail.com>
2021-07-29 01:27:54 -0400

    library toggle

--- src/components/Player.js
+++ src/components/Player.js
@@ -28,7 +28,7 @@ const Player = ({ currentSong, isPlaying, setIsPlaying, audioRef, setSongInfo, s
 				<p>{getTime(songInfo.currentTime)}</p>
 				<input
 					min={0}
-					max={songInfo.duration}
+					max={songInfo.duration || 0}
 					value={songInfo.currentTime}
 					onChange={dragHandler}
 					type="range"
