d69a214fb4d8191aece79a209330f5b8977e7316
Clifford Morin <cemorin21@gmail.com>
2021-07-29 03:32:03 -0400

    added custom slider from scratch, and slider animation

--- src/App.js
+++ src/App.js
@@ -18,14 +18,19 @@ function App() {
 	const [ isPlaying, setIsPlaying ] = useState(false);
 	const [ songInfo, setSongInfo ] = useState({
 		currentTime: 0,
-		duration: 0
+		duration: 0,
+		animationPercentage: 0
 	});
 	const [ libraryStatus, setLibraryStatus ] = useState(false);
 	//handler
 	const timeUpdateHandler = (e) => {
 		const current = e.target.currentTime;
 		const duration = e.target.duration;
-		setSongInfo({ ...songInfo, currentTime: current, duration: duration });
+		//Calculate percentage for animation
+		const roundedCurrent = Math.round(current);
+		const roundedDuration = Math.round(duration);
+		const animation = Math.round(roundedCurrent / roundedDuration * 100);
+		setSongInfo({ ...songInfo, currentTime: current, duration: duration, animationPercentage: animation });
 	};
 	return (
 		<div className="App">
